ef02aa1676da50b6ea5170e635ecf2e72cbdf1be
vedpaswan01 <vedpaswan01@gmail.com>
2022-11-11 20:50:32 +0530

    added exception and dao packages

---
+++ src/com/masai/dao/CustomerDao.java
@@ -0,0 +1,19 @@
+package com.masai.dao;
+
+import com.masai.exception.CustomerException;
+import com.masai.model.Customer;
+
+public interface CustomerDao {
+
+	public Customer loginCustomer (String userName, String password)throws CustomerException;
+	
+	public void transferAmount(int accNo,long ammount,int id,int cAcc) throws CustomerException;
+	
+	public void checkBalance(int id) throws CustomerException;
+	
+	public void transactionHistory(int cAcc) throws CustomerException;
+	
+	public String registerCustome(String name,String user_name,String password)throws CustomerException;
+	
+	//public String registerCustome(String first_name, String last_name, String user_name, String password,String mobile)throws CustomerException;
+}
